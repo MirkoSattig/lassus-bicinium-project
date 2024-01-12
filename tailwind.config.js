f27deb4544db1233ca8de4fd7bb880655a09e623
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-01-12 09:20:28 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                md: '3rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
+    },
+};
