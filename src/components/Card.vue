5281028997b85570ab0ef1248dcb79b388e9a12f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-01-12 09:55:22 +0100

    Add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div class="p-6 shadow border border-gray-100 rounded">
+        <div v-if="$slots.title || title">
+            <div class="flex items-center">
+                <div class="flex-grow">
+                    <slot name="title">
+                        <div class="text-xl font-medium leading-5 text-gray-800">
+                            {{ title }}
+                        </div>
+                    </slot>
+                </div>
+            </div>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
