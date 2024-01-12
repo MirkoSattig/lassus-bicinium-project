3c676ff452514850137c66e25f4555924fa130d1
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-01-12 09:56:24 +0100

    Translate nav item

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <NuxtLink to="/">Startseite</NuxtLink>
-        <NuxtLink to="/">About</NuxtLink>
+        <NuxtLink to="/">Über das Projekt</NuxtLink>
         <NuxtLink to="/bicinium">Bicinien</NuxtLink>
         <NuxtLink to="/cadences">Kadenzen</NuxtLink>
     </div>
