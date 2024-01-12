f5a1377e3fb79d3e2a48d53feb3e1887eeea0aeb
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-01-12 09:20:47 +0100

    Add container component

---
+++ src/components/Container.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    fluid: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<template>
+    <div :class="{ 'mx-auto px-2 md:px-4': fluid, container: !fluid }">
+        <slot></slot>
+    </div>
+</template>
