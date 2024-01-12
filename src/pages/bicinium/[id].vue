695b6a2e0ab38a5057853aff37113a855f81c2ee
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-01-12 09:55:30 +0100

    Add bicinium pages

---
+++ src/pages/bicinium/[id].vue
@@ -0,0 +1,17 @@
+<script setup>
+const { params: { id } } = useRoute();
+const { data: bicinium } = await useAsyncData(`bicinia/${id}`, () => queryContent(`/bicinia/${id}`).findOne());
+</script>
+
+<template>
+    <Container>
+        <Heading>Bicinien</Heading>
+        <VerovioCanvas
+            :url="bicinium.localRawFile"
+            :scale="35"
+            :options="{
+                spacingSystem: 15,
+            }"
+        />
+    </Container>
+</template>
