6388c7039989f2cd1c0222f99ce161db19f5425d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-02-02 16:53:04 +0100

    Push footer down

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-    <header>
-        <TheNavigation />
-    </header>
-    <main class="my-8">
-        <slot></slot>
-    </main>
-    <TheFooter />
+    <div class="flex flex-col min-h-screen">
+        <header>
+            <TheNavigation />
+        </header>
+        <main class="my-8">
+            <slot></slot>
+        </main>
+        <TheFooter class="mt-auto" />
+    </div>
 </template>
