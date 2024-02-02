e5cf3277456d227ddf4645e60cf07cbd7697e2bc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-02-02 16:52:27 +0100

    Add navigation component

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,36 @@
+<script setup>
+const { data: bicinia } = await useAsyncData(`bicinia/count`, () => queryContent(`/bicinia`).count());
+const { data: cadences } = await useAsyncData(`cadences/count`, () => queryContent(`/cadences`).count());
+const { data: imitations } = await useAsyncData(`imitations/count`, () => queryContent(`/imitations`).count());
+
+const links = [
+    {
+        label: 'Startseite',
+        to: '/',
+    },
+    {
+        label: 'Über das Projekt',
+        to: '/about',
+    },
+    {
+        label: 'Bicinien',
+        to: `/bicinium`,
+        badge: bicinia.value,
+    },
+    {
+        label: 'Kadenzen',
+        to: '/cadences',
+        badge: cadences.value,
+    },
+    {
+        label: 'Imitationen',
+        to: '/imitations',
+        badge: imitations.value,
+    },
+];
+</script>
+
+<template>
+    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+</template>
+
