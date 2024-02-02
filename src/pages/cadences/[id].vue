dee4fd2bd3461464643cfa31cfbecbd01d006662
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-02-02 16:53:49 +0100

    Add detail view for cadences

--- src/pages/cadences/[id].vue
+++ src/pages/cadences/[id].vue
@@ -1,3 +1,18 @@
+<script setup>
+const { params: { id } } = useRoute();
+const { data: cadence } = await useAsyncData(`cadences/${id}`, () => queryContent(`/cadences/${id}`).findOne());
+const { data: bicinium } = await useAsyncData(`bicinia/${cadence.value.biciniumId}`, () => queryContent(`/bicinia/${cadence.value.biciniumId}`).findOne());
+
+const location = `T. ${Math.ceil(cadence.value.endBeat / 8)} ♩ ${(cadence.value.endBeat % 8) + 1}`;
+</script>
+
 <template>
-    kadenz
+    <UContainer>
+        <Heading>
+            <NuxtLink :href="`/bicinium/${bicinium.id}`">
+                {{ `${bicinium.nr}. ${bicinium.title}, ${location}` }}
+            </NuxtLink>
+        </Heading>
+        <VerovioCanvas :url="`/kern/cadences/${cadence.filename}`" :scale="35" />
+    </UContainer>
 </template>
