50f55129703c97a919ce9cb5a81f64f41b5de2fc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-02-03 13:57:00 +0100

    Disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   srcDir:'src/',
   modules: [
     '@nuxt/content',
