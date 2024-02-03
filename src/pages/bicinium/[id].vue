758bd463cdf17379084be662294144c918c0b391
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-02-03 13:57:27 +0100

    Add links to GitHub and VHV

--- src/pages/bicinium/[id].vue
+++ src/pages/bicinium/[id].vue
@@ -6,12 +6,22 @@ const { data: bicinium } = await useAsyncData(`bicinia/${id}`, () => queryConten
 <template>
     <UContainer>
         <Heading>Bicinien</Heading>
-        <VerovioCanvas
-            :url="bicinium.localRawFile"
-            :scale="35"
-            :options="{
-                spacingSystem: 15,
-            }"
-        />
+        <div class="flex flex-col gap-4">
+            <div class="flex gap-2">
+                <UButton :to="`https://github.com/MirkoSattig/lassus-bicinia/blob/master/kern/${bicinium.id}.krn`" target="_blank">
+                    Auf GitHub öffnen
+                </UButton>
+                <UButton :to="`https://verovio.humdrum.org/?file=${encodeURIComponent(`verovio.https://github.com/MirkoSattig/lassus-bicinia/blob/master/kern/${bicinium.id}.krn`)}`" target="_blank">
+                    Im VHV öffnen
+                </UButton>
+            </div>
+            <VerovioCanvas
+                :url="bicinium.localRawFile"
+                :scale="35"
+                :options="{
+                    spacingSystem: 15,
+                }"
+            />
+        </div>
     </UContainer>
 </template>
