41d2656f8cd8626400c504943fdc738a44f87dce
Mirko Sattig <mirko.sattig@gmail.com>
2024-02-09 10:35:40 +0100

    edit about pages

---
+++ src/components/Subheading.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-3xl font-sans font-extrabold font-w mb-4">
+        <slot></slot>
+    </h1>
+</template>
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,24 +1,18 @@
 <template>
     <UContainer>
         <Heading>Über das Projekt</Heading>
-        <p>
-Über das Projekt
-
-Features
-alle 12 Bicinien können einzeln aufgerufen werden
-unter “Kadenzen” sind sämtliche Kadenzen aus den Bicinien gefiltert und einzeln einsehbar
-unter “Imitationen” lassen sich die einzelnen Soggetti, sowie deren Imitationsintervall herausfiltern und innerhalb der Partitur wiederfinden
-an der Erweiterung der Funktionen wird gearbeitet, Folgendes ist in nächster Zukunft geplant: Ergänzung der Bicinien um weitere, aus den Messen, Magnificats und Oratorien stammenden; Sortierfunktionen für die Kadenzen und Imitationen; grafisch anhand der Partitur hervorgehobene Analyse
-
-Humdrum
-Die Notenbeispiele für dieses Projekt wurden mit der Humdrum zugeordneten **kern representation notiert. Der Quellcode der Partituren für die Bicinien von Lassus ist in einem Repository auf GitHub verfügbar: https://github.com/MirkoSattig/lassus-bicinia.
-
-Verovio
-Die Humdrum-Parituren werden mit dem npm Package von Verovio in lesbare Partituren konvertiert. Um Verovio bequem mit dem JavaScript Framework Vue.js verwenden zu können wird vue-verovio-canvas als Dependency eingebunden.
-
-Vue.js / Nuxt
-Als technische Basis wird für diese Website das JavaScript Framework Nuxt verwendet das wiederum auf dem Framework Vue.js aufbaut. Der Quellcode dieser Website ist ebenfall auf GitHub verfügbar: https://github.com/MirkoSattig/lassus-bicinium-project.
-
-        </p>
+        <Subheading>Features</Subheading>
+        <ul>
+            <li>alle 12 Bicinien können einzeln aufgerufen werden</li>
+            <li>unter “Kadenzen” sind sämtliche Kadenzen aus den Bicinien gefiltert und einzeln einsehbar</li>
+            <li>unter “Imitationen” lassen sich die einzelnen Soggetti, sowie deren Imitationsintervall herausfiltern und innerhalb der Partitur wiederfinden</li>
+            <li>an der Erweiterung der Funktionen wird gearbeitet, Folgendes ist in nächster Zukunft geplant: Ergänzung der Bicinien um weitere, aus den Messen, Magnificats und Oratorien stammenden; Sortierfunktionen für die Kadenzen und Imitationen; grafisch anhand der Partitur hervorgehobene Analyse</li>
+        </ul>
+        <Subheading>Humdrum</Subheading>
+        <p>Die Notenbeispiele für dieses Projekt wurden mit der Humdrum zugeordneten **kern representation notiert. Der Quellcode der Partituren für die Bicinien von Lassus ist in einem Repository auf GitHub verfügbar: https://github.com/MirkoSattig/lassus-bicinia.</p>
+        <Subheading>Verovio</Subheading>
+        <p>Die Humdrum-Parituren werden mit dem npm Package von Verovio in lesbare Partituren konvertiert. Um Verovio bequem mit dem JavaScript Framework Vue.js verwenden zu können wird vue-verovio-canvas als Dependency eingebunden.</p>
+        <Subheading>Vue.js / Nuxt</Subheading>
+        <p>Als technische Basis wird für diese Website das JavaScript Framework Nuxt verwendet das wiederum auf dem Framework Vue.js aufbaut. Der Quellcode dieser Website ist ebenfall auf GitHub verfügbar: https://github.com/MirkoSattig/lassus-bicinium-project.</p>
     </UContainer>
 </template>
