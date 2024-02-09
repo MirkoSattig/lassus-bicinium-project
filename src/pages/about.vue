6ffd8db6e87158a62064e78d8a4ddf5a22902d0d
MirkoSattig <151973796+MirkoSattig@users.noreply.github.com>
2024-02-09 10:27:48 +0100

    Update about.vue

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,15 +2,23 @@
     <UContainer>
         <Heading>Über das Projekt</Heading>
         <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-            voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-            amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-            ipsum dolor sit amet.
+Über das Projekt
+
+Features
+alle 12 Bicinien können einzeln aufgerufen werden
+unter “Kadenzen” sind sämtliche Kadenzen aus den Bicinien gefiltert und einzeln einsehbar
+unter “Imitationen” lassen sich die einzelnen Soggetti, sowie deren Imitationsintervall herausfiltern und innerhalb der Partitur wiederfinden
+an der Erweiterung der Funktionen wird gearbeitet, Folgendes ist in nächster Zukunft geplant: Ergänzung der Bicinien um weitere, aus den Messen, Magnificats und Oratorien stammenden; Sortierfunktionen für die Kadenzen und Imitationen; grafisch anhand der Partitur hervorgehobene Analyse
+
+Humdrum
+Die Notenbeispiele für dieses Projekt wurden mit der Humdrum zugeordneten **kern representation notiert. Der Quellcode der Partituren für die Bicinien von Lassus ist in einem Repository auf GitHub verfügbar: https://github.com/MirkoSattig/lassus-bicinia.
+
+Verovio
+Die Humdrum-Parituren werden mit dem npm Package von Verovio in lesbare Partituren konvertiert. Um Verovio bequem mit dem JavaScript Framework Vue.js verwenden zu können wird vue-verovio-canvas als Dependency eingebunden.
+
+Vue.js / Nuxt
+Als technische Basis wird für diese Website das JavaScript Framework Nuxt verwendet das wiederum auf dem Framework Vue.js aufbaut. Der Quellcode dieser Website ist ebenfall auf GitHub verfügbar: https://github.com/MirkoSattig/lassus-bicinium-project.
+
         </p>
     </UContainer>
 </template>
