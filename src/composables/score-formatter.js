3f693c677be51cae8eb0fed288d01a1c9b84b370
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2024-02-10 14:57:31 +0100

    Add missing score formatter composable

---
+++ src/composables/score-formatter.js
@@ -0,0 +1,16 @@
+export function useScoreFormatter(kern) {
+    const options = useScoreOptions();
+    const score = ref(kern);
+
+    const formattedScore = computed(() => {
+        let str = score.value?.trim();
+        if (options.value.modernClefs) {
+            str += `\n!!!filter: modori -m`;
+        }
+        return str ?? null;
+    });
+
+    return {
+        formattedScore,
+    };
+};
